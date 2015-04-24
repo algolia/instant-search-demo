0fdb5635479a0dda1421016445587b835fdb15be
kokliKo <nicolas.baissas@gmail.com>
2015-04-24 14:51:20 +0200

    Helper v2

--- js/app.js
+++ js/app.js
@@ -1,143 +1,131 @@
-(function($) {
+$(document).ready(function() {
+
+
+  /********************
+  * INITIALIZATION
+  * *******************/
+
   // Client initialization
-    var algolia = new AlgoliaSearch('latency', '6be0576ff61c053d5f9a3225e2a90f76', {dsn: true, tld: 'net'}); // replace by your own ApplicationID and SearchableOnlyAPIKey
+  var algolia = algoliasearch('latency', '6be0576ff61c053d5f9a3225e2a90f76', {dsn: true, tld: 'net'}); // replace by your own ApplicationID and SearchableOnlyAPIKey
 
   // DOM binding
+  var $inputField = $('#q');
   var $hits = $('#hits');
-  var $pagination = $('#pagination');
   var $stats = $('#stats');
-  var $q = $('#q');
-  var $hitTemplate = Hogan.compile($('#hit-template').text());
-  var $statsTemplate = Hogan.compile($('#stats-template').text());
-  var $paginationTemplate = Hogan.compile($('#pagination-template').text());
   var $facets = $('#facets');
-  var $facetTemplate = Hogan.compile($('#facet-template').text());
-  var $sliderTemplate = Hogan.compile($('#slider-template').text());
+  var $pagination = $('#pagination');
+
+  // Templates binding
+  var hitTemplate = Hogan.compile($('#hit-template').text());
+  var statsTemplate = Hogan.compile($('#stats-template').text());
+  var facetTemplate = Hogan.compile($('#facet-template').text());
+  var sliderTemplate = Hogan.compile($('#slider-template').text());
+  var paginationTemplate = Hogan.compile($('#pagination-template').text());
+
+  // Initialize facets
+  var FACETS = [
+  { name: 'type',                title: 'Type',         disjunctive: false, sortFunction: sortByCountDesc },
+  { name: 'shipping',            title: 'Shipping',     disjunctive: false, sortFunction: sortByCountDesc },
+  { name: 'customerReviewCount', title: '# Reviews',    disjunctive: true,                                type: 'slider' },
+  { name: 'category',            title: 'Category',     disjunctive: true, sortFunction: sortByCountDesc, topListIfRefined: true },
+  { name: 'salePrice_range',     title: 'Price range',  disjunctive: true, sortFunction: sortByName },
+  { name: 'manufacturer',        title: 'Manufacturer', disjunctive: true, sortFunction: sortByName,      topListIfRefined: true }
+  ];
 
   // Helper initialization
-  var index = 'bestbuy'; // replace by your own index name
-  var helper = new AlgoliaSearchHelper(algolia, index, {
-    // list of conjunctive facets (link to refine)
-    facets: ['type', 'shipping', 'customerReviewCount'],
+  var indexName = 'bestbuy'; // replace by your own index name
+  var params = {
+    hitsPerPage: 10, // number of results per page
+    maxValuesPerFacet: 30, // number of facets values retrieved
+    facets: ['type', 'shipping'],
+    disjunctiveFacets: ['customerReviewCount', 'category', 'salePrice_range', 'manufacturer']
+  };
+  var helper = algoliasearchHelper(algolia, indexName, params);
 
-    // list of disjunctive facets (checkbox to refine)
-    disjunctiveFacets: ['category', 'salePrice_range', 'manufacturer'],
+  // Input binding
+  $inputField.on('keyup change', function() {
+    var query = $inputField.val();
+    toggleIconEmptyInput(!query.trim());
+    helper.setQuery(query).search();
+  }).focus();
 
-    // number of results per page
-    hitsPerPage: 10
+  // AlgoliaHelper events
+  helper.on("change", function(error) {
+    // update URL anchor
+    setURLParams(helper.state);
+  });
+  helper.on("error", function(error) {
+    console.log(error);
+  });
+  helper.on("result", function(content) {
+    var processedContent = processContent(content, this.state);
+    displayContent(processedContent);
+    bindSearchObjects();
   });
 
-  // Helpers
-  Number.prototype.numberWithDelimiter = function(delimiter) {
-    var number = this + '', delimiter = delimiter || ',';
-    var split = number.split('.');
-    split[0] = split[0].replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1' + delimiter);
-    return split.join('.');
-  };
 
-  // Facets ordered by order of display: here type will be displayed first and manufacture at the end
-  // Also includes the refinements initialization & configuration
-  function sortByCountDesc(a, b) { return b.count - a.count; }
-  function sortByNumAsc(a, b) { return parseInt(a.label) - parseInt(b.label); }
-  var FACETS = [
-  { name: 'type', title: 'Type', sortFunction: sortByCountDesc },
-  { name: 'shipping', title: 'Shipping', sortFunction: sortByCountDesc },
-  { name: 'customerReviewCount', title: 'Reviews' },
-  { name: 'category', title: 'Categories', sortFunction: sortByCountDesc, topListIfRefined: true },
-  { name: 'salePrice_range', title: 'Price', sortFunction: sortByNumAsc },
-  { name: 'manufacturer', title: 'Manufacturer', sortFunction: sortByNumAsc, topListIfRefined: true }
-  ];
-  var refinements = {};
-  var minReviewsCount = 0;
+  /************
+  * SEARCH
+  * ***********/
 
-  // Callback called on each keystroke, rendering the results
-  function searchCallback(success, content) {
-    if (!success || content.query !== $q.val()) {
-      // do not consider the result if there is an error
-      // or if it is outdated -> query != $q.val()
-      return;
-    }
+  // Initial search
+  initWithUrlParams();
+  helper.search();
 
-    // stats: render the number of results + processing time
-    $stats.html($statsTemplate.render({ query: content.query, nbHits: content.nbHits.numberWithDelimiter(), processingTimeMS: content.processingTimeMS, nbHits_plural: content.nbHits != 1 }));
+  // Process response sent by Algolia
+  function processContent(content, state) {
+    // Process stats
+    var stats =  {
+      nbHits: numberWithDelimiter(content.nbHits),
+      processingTimeMS: content.processingTimeMS,
+      nbHits_plural: content.nbHits !== 1
+    };
 
-    // hits: display the `hitsPerPage` results
-    var html = '';
-    for (var i = 0; i < content.hits.length; ++i) {
-      html += $hitTemplate.render(content.hits[i]);
-    }
-    $hits.html(html);
+    // Process hits
+    var hits = content.hits;
 
-    // facets: display the conjunctive+disjunctive facets
-    html = '';
-    var facetResult = null;
-    var facetConfig = null;
-    var isDisjunctive = null; 
-
-    for (var j=0; j<FACETS.length; ++j) {
-      facetConfig = FACETS[j];
-      facetResult = content['facets'][facetConfig.name] || content['disjunctiveFacets'][facetConfig.name] || null;
-      isDisjunctive = (content['disjunctiveFacets'][facetConfig.name]) ? true : false;
-
+    // Process facets
+    var facets = [];
+    for (var facetIndex = 0; facetIndex < FACETS.length; ++facetIndex) {
+      var facetParams = FACETS[facetIndex];
+      var facetResult = content.getFacetByName(facetParams.name);
       if (facetResult) {
-        
-        if (facetConfig.name === 'customerReviewCount') {
-          // add a slider fetching the 'max' value of 'customerReviewCount' from `content.facets_stats.customerReviewCount`
-          html += $sliderTemplate.render({ facet: facetConfig.name, title: facetConfig.title, max: content.facets_stats.customerReviewCount.max, current: minReviewsCount });
+        var facetContent = {};
+        facetContent.facet = facetParams.name;
+        facetContent.title = facetParams.title;
+        facetContent.type = facetParams.type;
+
+        // if the facet is a slider
+        if (facetParams.type === 'slider') {
+          facetContent.min = facetResult.stats.min;
+          facetContent.max = facetResult.stats.max;
+          var valueMin = state.getNumericRefinement("customerReviewCount", ">=") || facetResult.stats.min;
+          var valueMax = state.getNumericRefinement("customerReviewCount", "<=") || facetResult.stats.max;
+          valueMin = Math.min(facetContent.max, Math.max(facetContent.min, valueMin));
+          valueMax = Math.min(facetContent.max, Math.max(facetContent.min, valueMax));
+          facetContent.values = [valueMin, valueMax];
         } else {
-          // other facets
 
-          // collect all values from `facetResult` to sort them by facetConfig.sortFunction
+          // format and sort the facet values
           var values = [];
-          for (var v in facetResult) {
-            values.push({ label: v, value: v, count: facetResult[v], refined: helper.isRefined(facetConfig.name, v) });
+          for (var v in facetResult.data) {
+            values.push({ label: v, value: v, count: facetResult.data[v], refined: helper.isRefined(facetParams.name, v) });
           }
-          // sort the values
-          values.sort(function(a, b) {
-            // If topListIfRefined === true: sort by the refined states first (put them on top if they are refined).
-            if (facetConfig.topListIfRefined) {
-              if (a.refined !== b.refined) {
-                if (a.refined) return -1;
-                if (b.refined) return 1;
-              }
-            }
+          var sortFunction = facetParams.sortFunction || sortByCountDesc;
+          if (facetParams.topListIfRefined) sortFunction = sortByRefined(sortFunction);
+          values.sort(sortFunction);
 
-            // then fallback on the standard sort function
-            return facetConfig.sortFunction(a,b);
-          });
+          facetContent.values = values.slice(0, 10);
+          facetContent.has_other_values = values.length > 10;
+          facetContent.other_values = values.slice(10);
+          facetContent.disjunctive = facetParams.disjunctive;
 
-          // render the facet
-          html += $facetTemplate.render({
-            facet: facetConfig.name,
-            title: facetConfig.title,
-            values: values.slice(0, 10),
-            has_other_values: values.length > 10,
-            other_values: values.slice(10),
-            disjunctive: isDisjunctive
-          });
         }
-        
+        facets.push(facetContent);
       }
     }
-    $facets.html(html);
 
-    // bind slider
-    $('#customerReviewCount-slider').slider({
-      formater: function(e) {
-        if (e === 0) {
-          return 'All';
-        }
-        return '> ' + e;
-      }
-    }).on('slideStop', function(ev) {
-      minReviewsCount = ev.value;
-      search();
-    });
-
-    // pimp checkboxes
-    $('input[type="checkbox"]').checkbox();
-
-    // render pagination
+    // Process pagination
     var pages = [];
     if (content.page > 5) {
       pages.push({ current: false, number: 1 });
@@ -147,116 +135,156 @@
       if (p < 0 || p >= content.nbPages) {
         continue;
       }
-      pages.push({ current: content.page == p, number: (p + 1) });
+      pages.push({ current: content.page === p, number: (p + 1) });
     }
     if (content.page + 5 < content.nbPages) {
       pages.push({ current: false, number: '...', disabled: true });
       pages.push({ current: false, number: content.nbPages });
     }
-    $pagination.html($paginationTemplate.render({ pages: pages, prev_page: (content.page > 0 ? content.page : false), next_page: (content.page + 1 < content.nbPages ? content.page + 2 : false) }));
-
-    // update URL anchor
-    var refinements = [];
-    for (var refine in helper.refinements) {
-      if (helper.refinements[refine]) {
-        var i = refine.indexOf(':');
-        var r = {};
-        r[refine.slice(0, i)] = refine.slice(i + 1);
-        refinements.push(r);
-      }
-    }
-    for (var refine in helper.disjunctiveRefinements) {
-      for (var value in helper.disjunctiveRefinements[refine]) {
-        if (helper.disjunctiveRefinements[refine][value]) {
-          var r = {};
-          r[refine] = value;
-          refinements.push(r);
-        }
-      }
-    }
-    location.replace('#q=' + encodeURIComponent(content.query) + '&page=' + content.page + '&minReviewsCount=' + minReviewsCount + '&refinements=' + encodeURIComponent(JSON.stringify(refinements)));
-
-    // scroll on top
-    window.scrollTo(0, 0);
-  }
-
-  // perform a search
-  function search() {
-    var params = {
-      // retrieve maximum 50 values per facet to display the "more" link
-      maxValuesPerFacet: 50
+    var pagination = {
+      pages: pages,
+      prev_page: (content.page > 0 ? content.page : false),
+      next_page: (content.page + 1 < content.nbPages ? content.page + 2 : false)
     };
-    // plug review_count slider refinement
-    if (minReviewsCount > 0) {
-      params.numericFilters = 'customerReviewCount>=' + minReviewsCount;
-    }
-    // if we're sorting by something,
-    // make the typo-tolerance more strict
-    if (helper.index != index) {
-      // disable if not the "default" index (sort by price, etc...)
-      params.typoTolerance = false;
-    }
-    // perform the query
-    helper.search($q.val(), searchCallback, params);
+
+    return {
+      stats: stats,
+      hits: hits,
+      facets: facets,
+      pagination: pagination
+    };
   }
 
-  // init: fetch anchor params and init the associated variables
-  if (location.hash && location.hash.indexOf('#q=') === 0) {
-    var params = location.hash.substring(3);
-    var pageParamOffset = params.indexOf('&page=');
-    var minReviewsCountParamOffset = params.indexOf('&minReviewsCount=');
-    var refinementsParamOffset = params.indexOf('&refinements=');
+  // Display the content on the page
+  function displayContent(content) {
+    // Display stats
+    $stats.html(statsTemplate.render(content.stats));
 
-    var q = decodeURIComponent(params.substring(0, pageParamOffset));
-    var page = parseInt(params.substring(pageParamOffset + 6, minReviewsCountParamOffset));
-    minReviewsCount = parseInt(params.substring(minReviewsCountParamOffset + 17, refinementsParamOffset));
-    var refinements = JSON.parse(decodeURIComponent(params.substring(refinementsParamOffset + 13)));
+    // Display hits
+    var hitsHtml = '';
+    for (var i = 0; i < content.hits.length; ++i) {
+      hitsHtml += hitTemplate.render(content.hits[i]);
+    }
+    if (content.hits.length === 0) hitsHtml = '<p id="no-hits">We didn\'t find any products for your search.</p>';
+    $hits.html(hitsHtml);
 
-    $q.val(q);
-    for (var i = 0; i < refinements.length; ++i) {
-      for (var refine in refinements[i]) {
-        helper.toggleRefine(refine, refinements[i][refine]);
+    // Display facets
+    var facetsHtml = '';
+    for (var indexFacet = 0; indexFacet < content.facets.length; ++indexFacet) {
+      var facet = content.facets[indexFacet];
+      if (facet.type && facet.type === 'slider') facetsHtml += sliderTemplate.render(facet);
+      else facetsHtml += facetTemplate.render(facet);
+    }
+    $facets.html(facetsHtml);
+    if (content.hits.length === 0) $facets.empty();
+
+    // Display pagination
+    $pagination.html(paginationTemplate.render(content.pagination));
+    if (content.hits.length === 0) $pagination.empty();
+  }
+
+  // Event bindings
+  function bindSearchObjects() {
+    // Slider binding
+    $('#customerReviewCount-slider').slider().on('slideStop', function(ev) {
+      helper.addNumericRefinement("customerReviewCount", ">=", ev.value[0]).search();
+      helper.addNumericRefinement("customerReviewCount", "<=", ev.value[1]).search();
+    });
+
+    // Pimp checkboxes
+    $('input[type="checkbox"]').checkbox(); 
+  }
+
+  // Click binding
+  $(document).on("click",".show-more, .show-less",function() {
+    $(this).closest('ul').find('.show-more').toggle();
+    $(this).closest('ul').find('.show-less').toggle();
+  });
+  $(document).on("click",".toggleRefine",function() {
+    helper.toggleRefine($(this).data("facet"), $(this).data("value")).search();
+  });
+  $(document).on("click",".gotoPage",function() {
+    helper.setCurrentPage(+$(this).data("page") - 1).search();
+    window.scrollTo(0, 0);
+  });
+  $(document).on("click",".sortBy",function() {
+    $(this).closest('.btn-group').find('.sort-by').text($(this).text());
+    helper.setIndex(indexName + $(this).data("index-suffix")).search(); // Todo remove when new helper
+  });
+  $(document).on("click","#input-loop",function() {
+    $inputField.val("").change();
+  });
+
+
+  /************
+  * TOOLS
+  * ***********/
+
+  function toggleIconEmptyInput(isEmpty) {
+    if(isEmpty) {
+      $('#input-loop').addClass("glyphicon-loop");
+      $('#input-loop').removeClass("glyphicon-remove");
+    }
+    else {
+      $('#input-loop').removeClass("glyphicon-loop");
+      $('#input-loop').addClass("glyphicon-remove");
+    }
+  }
+  function numberWithDelimiter(number, delimiter) {
+    number = number + '';
+    delimiter = delimiter || ',';
+    var split = number.split('.');
+    split[0] = split[0].replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1' + delimiter);
+    return split.join('.');
+  }
+  var sortByCountDesc = function sortByCountDesc (a, b) { return b.count - a.count; };
+  var sortByName = function sortByName (a, b) {
+    return a.value.localeCompare(b.value);
+  };
+  var sortByRefined = function sortByRefined (sortFunction) {
+    return function (a, b) {
+      if (a.refined !== b.refined) {
+        if (a.refined) return -1;
+        if (b.refined) return 1;
+      }
+      return sortFunction(a, b);
+    };
+  };
+  function initWithUrlParams() {
+    var sPageURL = location.hash;
+    if (!sPageURL || sPageURL.length === 0) { return true; }
+    var sURLVariables = sPageURL.split('&');
+    if (!sURLVariables || sURLVariables.length === 0) { return true; }
+    var query = decodeURIComponent(sURLVariables[0].split('=')[1]);
+    $inputField.val(query);
+    helper.setQuery(query);
+    for (var i = 2; i < sURLVariables.length; i++) {
+      var sParameterName = sURLVariables[i].split('=');
+      var facet = decodeURIComponent(sParameterName[0]);
+      var value = decodeURIComponent(sParameterName[1]);
+      helper.toggleRefine(facets, value, false);
+    }
+    // Page has to be set in the end to avoid being overwritten
+    var page = decodeURIComponent(sURLVariables[1].split('=')[1])-1;
+    helper.setCurrentPage(page);
+
+  }
+  function setURLParams(state) {
+    var urlParams = "#";
+    var currentQuery = state.query;
+    urlParams += "q=" + encodeURIComponent(currentQuery);
+    var currentPage = state.page+1;
+    urlParams += "&page=" + currentPage;
+    for (var facetRefine in state.facetsRefinements) {
+      urlParams += "&" + encodeURIComponent(facetRefine) + "=" + encodeURIComponent(state.facetsRefinements[facetRefine]);
+    }
+    for (var disjunctiveFacetrefine in state.disjunctiveFacetsRefinements) {
+      for (var value in state.disjunctiveFacetsRefinements[disjunctiveFacetrefine]) {
+        urlParams += "&" + encodeURIComponent(disjunctiveFacetrefine) + "=" + encodeURIComponent(state.disjunctiveFacetsRefinements[disjunctiveFacetrefine][value]);
       }
     }
-    helper.setPage(page);
+    location.replace(urlParams);
   }
 
-  // input binding
-  var lastQuery = $q.val();
-  $q.on('keyup change', function() {
-    if ($q.val() != lastQuery) {
-      lastQuery = $q.val();
-      // performing a new full-text query reset the pagination and the refinements
-      minReviewsCount = 0;
-      helper.setPage(0);
-      helper.clearRefinements();
-      search();
-    }
-  }).focus();
+});
 
-  // load results
-  search();
-
-  // click binding
-  window.showMoreLess = function(link) {
-    $(link).closest('ul').find('.show-more').toggle();
-  };
-  window.toggleRefine = function(facet, value) {
-    // refinining a facet reset the pagination
-    helper.setPage(0);
-    helper.toggleRefine(facet, value);
-  };
-  window.gotoPage = function(page) {
-    helper.gotoPage(+page - 1);
-  };
-  window.sortBy = function(index_suffic, link) {
-    $(link).closest('.btn-group').find('.sort-by').text($(link).text());
-    // set target index name
-    helper.index = index + index_suffic;
-    // reset page
-    helper.setPage(0);
-    // perform the query
-    search();
-  };
-
-})(jQuery);

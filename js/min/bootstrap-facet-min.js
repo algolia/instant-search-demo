e4495602c394b45376d578c9acfcec7844bf1972
vvo <vincent.voyer@algolia.com>
2015-04-27 11:17:52 +0200

    cleanup

--- js/min/bootstrap-facet-min.js
+++
@@ -1,1 +0,0 @@
-!function($){$("#facets").on("mouseenter mouseleave",".button-checkbox",function(e){$(this).parent().find(".facet_link").toggleClass("hover")}),$("#facets").on("mouseenter mouseleave",".facet_link",function(e){$(this).parent().find(".button-checkbox button.btn").toggleClass("hover")})}(jQuery);
